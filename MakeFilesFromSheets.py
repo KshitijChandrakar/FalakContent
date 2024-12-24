95b1b0282f5bf5667593a874150f34843303a0ef
kshitij <kshitijchandrakar2019@gmail.com>
2024-12-24 20:09:38 +0530

    pushing-content

--- MakeFilesFromSheets.py
+++ MakeFilesFromSheets.py
@@ -4,8 +4,7 @@ with open("results.json", "r") as results:
     results = json.load(results)['results'][0]['result']['formatted']
     # print(results)
     for i in results:
-        path = f"en/post/{i["(B)"]}-{i["(C)"]}.md"
-        path = os.path.join(path, file)
+        path = os.pathf"en/post/{i["(B)"]}-{i["(C)"]}.md"
         if not os.path.exists(path) and i["(G)"].lower() in ["true", "yes", "1"]: #Somehow communicate to the author that the post exists
             print(f"yea it doesnt exist, going ahead with making file {path}")
             with open(path, "w") as file:
