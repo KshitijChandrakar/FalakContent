94f5c193a2fdbb89d4b484beed157f5c55c47551
kshitij <kshitijchandrakar2019@gmail.com>
2024-12-24 20:01:53 +0530

    pushing-content

--- MakeFilesFromSheets.py
+++ MakeFilesFromSheets.py
@@ -5,12 +5,13 @@ with open("results.json", "r") as results:
     # print(results)
     for i in results:
         path = f"{i["(B)"]}-{i["(C)"]}.md"
-        if not os.path.exists(path) and i["(G)"].lower() in ["true", "yes", "1"] : #Somehow communicate to the author that the post exists
+        if not os.path.exists(path) and i["(G)"].lower() in ["true", "yes", "1"]: #Somehow communicate to the author that the post exists
+            print("yea it doesnt exist, going ahead with making it")
             with open(path, "w") as file:
-                file.write("---")
-                file.write(f"title: '{i["(C)"]}'")
-                file.write(f"date: '{i["(D)"]}'")
-                file.write(f"author: '{i["(B)"]}'")
-                file.write("---")
+                file.write("---\n")
+                file.write(f"title: '{i["(C)"]}'\n")
+                file.write(f"date: '{i["(D)"]}'\n")
+                file.write(f"author: '{i["(B)"]}'\n")
+                file.write("---\n")
                 file.write(i["(E)"])
     pass
